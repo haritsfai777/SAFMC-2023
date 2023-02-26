f0d0ab92612f03ac9050c1d43ac707f4adb7bd56
Hafizh <hafizhrenanto88@gmail.com>
2023-02-26 11:02:16 +0700

    Little formatting

--- movement_script.py
+++ movement_script.py
@@ -65,124 +65,41 @@ def set_velocity_body(vehicle, vx, vy, vz):
     vehicle.send_mavlink(msg)
     vehicle.flush()
     
+# Get speed for decelaring
 def get_speed(current_pos):
-    # For decelerating speed
     if (current_pos > 0):
-        #for positive x value (right)
+        # for positive x value (right)
         return -0.125(current_pos-2)^2 + 0.5
     else:
-        #for negative x value (left)
-        return -0.125(current_pos-2)^2 - 0.5
+        # for negative x value (left)
+        return 0.125(current_pos-2)^2 - 0.5
 
-#-- Move from aruco
+# Give the drone velocity vector. This function should be called every new x and y value is inputted from aruco detector
 def gerakDrone(x, y):
-    # Check x position relative to the next aruco
-    if (x >= 2):
-        set_velocity_body(vehicle, gnd_speed, 0, 0)
-    elif (x <= -2):
-        set_velocity_body(vehicle, -gnd_speed, 0, 0)
-    elif (x < 2 and x >= 0):
-        #decelerates velocity down to 0
+    # First, we check the current x position relative to the aruco and move in the x axis based on that position
+    # Here, we set x = 2 m as decelerating threshold
+    if (abs(x) >= 2):
+        set_velocity_body(vehicle, gnd_speed * x / abs(x), 0, 0)
+    else:
         set_velocity_body(vehicle, get_speed(x), 0, 0)
-        #once velocity has reached 0
-        if (-0.1 < x < 0.1):
-            # After we reached the x threshold, stop x velocity and move y function
-            if (y >= 2):
-                set_velocity_body(vehicle, 0, gnd_speed, 0)
-            elif (y <= -2):
-                set_velocity_body(vehicle, 0, -gnd_speed, 0)
-            elif (y < 2 and y >= 0):
-                set_velocity_body(vehicle, 0, get_speed(y), 0)
-            elif (y > -2 and y <= 0):
-                set_velocity_body(vehicle, 0, get_speed(y), 0)
-    elif (x > -2 and x <= 0):
-        set_velocity_body(vehicle, get_speed(x), 0, 0)
-        if (-0.1 < x < 0.1):
-            if (y >= 2):
-                set_velocity_body(vehicle, 0, gnd_speed, 0)
-            elif (y <= -2):
-                set_velocity_body(vehicle, 0, -gnd_speed, 0)
-            elif (y < 2 and y >= 0):
-                set_velocity_body(vehicle, 0, get_speed(y), 0)
-            elif (y > -2 and y <= 0):
+
+        # Here, we set x = 0.1 as x position tolerance before the drone starts moving in y direction
+        if(abs(x) > 0.1):
+            if(abs(y) >= 2):
+                set_velocity_body(vehicle, 0, gnd_speed * y / abs(y), 0)
+            else:
                 set_velocity_body(vehicle, 0, get_speed(y), 0)
 
-# # --Move drone based on input from aruco with PID
-# setpoint    = 0
-
-# error       = 0
-# integral    = 0
-# derivative  = 0
-
-# error_prev  = 0
-
-# dt          = 0.01                  # data time sampling
-
-# # need tuning
-# Kp          = 0.05                  # proportional coefficient
-# Ki          = 0.0005                # integral coefficient
-# Kd          = 0.0000000005          # derivative coefficient
-
-# giliran_x_gerak = True
-
-# def gerakDronePID(x, y):
-#     global error_prev, integral, derivative, giliran_x_gerak
-
-#     if (giliran_x_gerak):
-#         if (abs(x) > 0):
-#             set_velocity_body(vehicle, getSpeedPID(x), 0, 0)
-#         else:
-#             set_velocity_body(vehicle, 0, 0, 0)
-
-#             error_prev = 0
-#             integral = 0
-#             derivative = 0
-
-#             giliran_x_gerak = False
-#     else:
-#         if (abs(y) > 0):
-#             set_velocity_body(vehicle, 0, getSpeedPID(y), 0)
-#         else:
-#             set_velocity_body(vehicle, 0, 0, 0)
-
-#             error_prev = 0
-#             integral = 0
-#             derivative = 0
-
-#             giliran_x_gerak = True 
-
-# def getSpeedPID(feedback):
-#     global error_prev, integral, derivative
-
-#     # Get the error
-#     error = feedback - setpoint
-#     de = error - error_prev
-
-#     # Summate the integral
-#     integral = integral + (error + error_prev) * dt / 2           # trapezoidal rule
-
-#     # Get the derivative
-#     derivative = de / dt
-
-#     # Get the speed
-#     speed = Kp * error + Ki * integral + Kd * derivative
-
-#     # Simpan previous error
-#     error_prev = error
-
-#     return max(speed, gnd_speed)
-
+# Used only if x and y position is not detected, manually set the direction based on command from the previous aruco marker
 def gerakDroneEmergency(string):
-    # Used only if x and y position is not detected, manually set the direction based on command from the previous aruco marker
     if string == "FORWARD":
         set_velocity_body(vehicle, 0, gnd_speed, 0)
-    elif string == "BACK":
+    elif string == "BACKWARD":
         set_velocity_body(vehicle, 0, -gnd_speed, 0)
     elif string == "RIGHT":
         set_velocity_body(vehicle, gnd_speed, 0, 0)
     elif string == "LEFT":
         set_velocity_body(vehicle, -gnd_speed, 0, 0)
-    
     
 #---- MAIN FUNCTION
 #- Takeoff
